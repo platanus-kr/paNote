bf4a518ec83621fcf6a5eed3da474c98796f9ebf
platanus <platanus@canxan.com>
2021-03-18 09:59:20 +0900

    fix(template): Change post date showing absolute again

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -457,10 +457,10 @@ if ( ! function_exists( 'mynote_author_posted_date' ) ) {
 				esc_html( get_the_date() ), 
 				esc_attr( get_the_time() )
 			),
-			get_the_author(),
+			// get_the_author(),
 			get_the_time( 'c' ),
 			sprintf( 
-				_x( ' / post date %s', '%s', 'mynote' ), 
+				_x( 'ost date %s', '%s', 'mynote' ), 
 				// get_the_date( 'Y, F j (l)' )
 				get_the_date( 'Y.m.d H:i A' )
 				// date("Y.m.d",get_the_time( 'U' ))
